97745929d0a6c5c31a952ab6cdbc96ab41e39d70
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-12-01 18:10:14 -0300

    wip: fix test route

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,20 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('test', function() {
+
+    $user = User::first();
+
+    if(!$user) {
+        $user = User::factory()->create();
+    }
+
     $reminder = Reminder::first();
+
+    if(!$reminder) {
+        $reminder = Reminder::factory()->create([
+            'user_id' => $user->id,
+        ]);
+    }
 
     NotifyReminderEvent::dispatch($reminder);
 });
