b1867c6dbbef2ae90be5dbbba9bdaab7acf9fd06
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-12-01 15:48:33 -0300

    test: add database assertion

--- tests/Feature/app/Console/Commands/DispatchReminderCommandTest.php
+++ tests/Feature/app/Console/Commands/DispatchReminderCommandTest.php
@@ -19,6 +19,13 @@ it('should dispatch the available reminders 10 minutes before the scheduled time
 
     $this->artisan('app:dispatch-reminder-command');
 
+    $this->assertDatabaseHas('reminders', [
+        'id' => $reminder->id,
+        'current_step' => 1,
+        'is_notified' => false,
+        'notified_at' => null,
+    ]);
+
     Queue::assertPushed(DispatchReminderJob::class, function (DispatchReminderJob $job) use ($reminder) {
         return $job->reminder->id === $reminder->id;
     });
@@ -28,12 +35,21 @@ it('should not dispatch the reminders that are already notified', function () {
     Queue::fake();
 
     $user = User::factory()->create();
-    Reminder::factory()->count(1)->create([
+    $reminder = Reminder::factory()->create([
         'user_id' => $user->id,
         'is_notified' => true,
+        'current_step' => 2,
+        'notified_at' => now(),
     ]);
 
     $this->artisan('app:dispatch-reminder-command');
+
+    $this->assertDatabaseHas('reminders', [
+        'id' => $reminder->id,
+        'current_step' => 2,
+        'is_notified' => true,
+        'notified_at' => now(),
+    ]);
 
     Queue::assertNotPushed(DispatchReminderJob::class);
 });
@@ -43,12 +59,19 @@ it('should not dispatch the reminders if the scheduled time is more than 10 minu
     Queue::fake();
 
     $user = User::factory()->create();
-    Reminder::factory()->create([
+    $reminder = Reminder::factory()->create([
         'user_id' => $user->id,
         'scheduled_at' => now()->addMinutes(20),
     ]);
 
     $this->artisan('app:dispatch-reminder-command');
+
+    $this->assertDatabaseHas('reminders', [
+        'id' => $reminder->id,
+        'current_step' => 0,
+        'is_notified' => false,
+        'notified_at' => null,
+    ]);
 
     Queue::assertNotPushed(DispatchReminderJob::class);
 });
